a6e0738c298a99c909d64415d00abad843557ef1
Kamil Wrobel <wrobelk@live.com>
2023-11-22 08:57:46 -0500

    Adds a template page

---
+++ NeetCode150.playground/Pages/141. Linked List Cycle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 141. Linked List Cycle
+
+// MARK: - My Solution
+
+// MARK: - Other Solutions
+
+// MARK: - Tests
+import XCTest
+
+class LinkedListCycleTests: XCTestCase {
+    func testExample() {
+        
+    }
+}
+
+LinkedListCycleTests.defaultTestSuite.run()
+
+//: [Next](@next)
---
+++ NeetCode150.playground/Pages/TEMPLATE.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// #. Challenge Name
+
+// MARK: - My Solution
+
+
+// MARK: - Other Solutions
+
+
+// MARK: - Tests
+import XCTest
+
+class Name: XCTestCase {
+    func testExample() {
+        
+    }
+}
+
+Name.defaultTestSuite.run()
+
+//: [Next](@next)
