01e6278a6aeb364661cb4d58f46ca381a8585719
Kamil Wrobel <wrobelk@live.com>
2023-11-27 13:27:24 -0500

    Merge two sorted linked list solution

--- NeetCode150.playground/Pages/21. Merge Two Sorted Linked Lists - Easy.xcplaygroundpage/Contents.swift
+++ NeetCode150.playground/Pages/21. Merge Two Sorted Linked Lists - Easy.xcplaygroundpage/Contents.swift
@@ -25,29 +25,101 @@ class ListNode {
 }
 
 // MARK: - My Solution
-func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-    var mergedList: ListNode? = list1
-    var currentNode1 = list1
-    var currentNode2 = list2
+// This solutions fails on some tests
+//func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+//    var currentNode1 = list1
+//    var currentNode2 = list2
+//    var currentNode: ListNode? = ListNode()
+//    let mergedList = currentNode
+//    
+//    while currentNode1 != nil {
+//        if let value1 = currentNode1?.val, let value2 = currentNode2?.val {
+//            if value1 <= value2 {
+//                currentNode?.next = currentNode1
+//                currentNode1 = currentNode1?.next
+//            } else {
+//                currentNode?.next = currentNode2
+//                currentNode2 = currentNode2?.next
+//            }
+//            currentNode = currentNode?.next
+//        }
+//    }
+//    
+//    currentNode?.next = currentNode1 != nil ? currentNode1 : currentNode2
+//    
+//    return mergedList?.next
+//}
+
+func mergeTwoLists2(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    var mergedList: ListNode? = ListNode()
+    var currentNode : ListNode? = mergedList
+    var node1 = l1
+    var node2 = l2
     
-    while currentNode1?.next != nil && currentNode2?.next != nil {
-        if let value1 = currentNode1?.val, let value2 = currentNode2?.val {
-            
-            if value1 <= value2 {
-                mergedList?.next = currentNode1
-                currentNode1 = currentNode1?.next
-            } else {
-                mergedList?.next = currentNode2
-                currentNode2 = currentNode2?.next
-            }
+    while node1 != nil && node2 != nil {
+        if node1!.val <= node2!.val {
+            currentNode?.next = node1
+            node1 = node1?.next
+        } else {
+            currentNode?.next = node2
+            node2 = node2?.next
         }
+        currentNode = currentNode?.next
+    }
+    
+    if node1 == nil {
+        currentNode?.next = node2
+    }
+    
+    if node2 == nil {
+        currentNode?.next = node1
     }
     
     return mergedList?.next
 }
 
-// MARK: - Other Solutions
 
+
+// MARK: - Other Solutions
+//func mergeTwoLists2(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+//    var list1 = list1
+//    var list2 = list2
+//    let dummy: ListNode? = ListNode()
+//    var node = dummy
+//    
+//    while list1 != nil, list2 != nil {
+//        if let value1 = list1?.val, let value2 = list2?.val {
+//            if value1 < value2 {
+//                node?.next = list1
+//                list1 = list1?.next
+//            } else{
+//                node?.next = list2
+//                list2 = list2?.next
+//            }
+//        }
+//        
+//        node = node?.next
+//    }
+//    
+//    node?.next = list1 != nil ? list1 : list2
+//    return dummy?.next
+//
+//
+
+// Recursive Solution
+func mergeTwoLists3(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+    if list1 == nil || list2 == nil {
+        return list1 == nil ? list2 : list1
+    }
+    
+    if list1!.val <= list2!.val {
+        list1?.next = mergeTwoLists3(list1?.next, list2)
+        return list1
+    } else {
+        list2?.next = mergeTwoLists3(list1, list2?.next)
+        return list2
+    }
+}
 
 // MARK: - Tests
 import XCTest
@@ -59,15 +131,39 @@ class MergeTwoSortedLikedListsTest: XCTestCase {
         let linkedListOne = parse(inputOne)
         let linkedListTwo = parse(inputTwo)
         
-        let merged = mergeTwoLists(linkedListOne, linkedListTwo)
+//        let merged = mergeTwoLists(linkedListOne, linkedListTwo)
+//        let merged2 = mergeTwoLists2(linkedListOne, linkedListTwo)
+        let merged3 = mergeTwoLists3(linkedListOne, linkedListTwo)
         
-        XCTAssertEqual(merged?.description, "1 -> 1 -> 2 -> 2 -> 2 -> 3 -> 4 -> 5 -> 6")
+//        XCTAssertEqual(merged?.description, "1 -> 1 -> 2 -> 2 -> 2 -> 3 -> 4 -> 5 -> 6")
+//        XCTAssertEqual(merged2?.description, "1 -> 1 -> 2 -> 2 -> 2 -> 3 -> 4 -> 5 -> 6")
+        XCTAssertEqual(merged3?.description, "1 -> 1 -> 2 -> 2 -> 2 -> 3 -> 4 -> 5 -> 6")
+    }
+    
+    func testWithOneEmptyList() {
+        let inputOne = [1]
+        let inputTwo: [Int] = []
+        let linkedListOne = parse(inputOne)
+        let linkedListTwo = parse(inputTwo)
+        
+//        let merged = mergeTwoLists(linkedListOne, linkedListTwo)
+//        let merged2 = mergeTwoLists2(linkedListOne, linkedListTwo)
+        let merged3 = mergeTwoLists3(linkedListOne, linkedListTwo)
+        
+//        XCTAssertEqual(merged?.description, "1")
+//        XCTAssertEqual(merged2?.description, "1")
+        XCTAssertEqual(merged3?.description, "1")
+        
     }
 }
 
 class HelpersTests: XCTestCase {
     func testParsingArrayToLinkedList() {
-        XCTAssertEqual(parse([1,2,3,4,5]).description, "1 -> 2 -> 3 -> 4 -> 5")
+        XCTAssertEqual(parse([1,2,3,4,5])?.description, "1 -> 2 -> 3 -> 4 -> 5")
+    }
+    
+    func testParsingEmptyArray() {
+        XCTAssertNil(parse([])?.description)
     }
     
     func testConvertingLinkedListToArray() {
@@ -78,10 +174,21 @@ class HelpersTests: XCTestCase {
         
         XCTAssertEqual(inputArray, returnedArray)
     }
+    
+    func testConvertingEmptyLinkedListToArray() {
+        let inputArray: [Int] = []
+        let linkedList = parse(inputArray)
+        
+        let returnedArray = convert(linkedList: linkedList)
+        
+        XCTAssertEqual(inputArray, returnedArray)
+    }
 }
 
 // MARK: -  Helpers
-func parse(_ numbers: [Int]) -> ListNode {
+func parse(_ numbers: [Int]) -> ListNode? {
+    guard !numbers.isEmpty else { return nil }
+    
     var currentNode: ListNode = ListNode(999)
     let node: ListNode = currentNode
     
@@ -91,19 +198,27 @@ func parse(_ numbers: [Int]) -> ListNode {
         currentNode = currentNode.next!
     }
     
-    return node.next ?? ListNode(000)
+    return node.next
 }
 
-func convert(linkedList node: ListNode) -> [Int] {
+func convert(linkedList node: ListNode?) -> [Int] {
     var currentNode = node
     var numbers: [Int] = []
-    
-    while currentNode.next != nil {
-        numbers.append(currentNode.val)
-        currentNode = currentNode.next!
+
+    if node == nil {
+        return numbers
     }
     
-    numbers.append(currentNode.val)
+    while currentNode?.next != nil {
+        if let val = currentNode?.val {
+            numbers.append(val)
+            currentNode = currentNode?.next!
+        }
+    }
+    
+    if let val = currentNode?.val {
+        numbers.append(val)
+    }
     
     return numbers
 }
@@ -118,6 +233,7 @@ extension ListNode: CustomStringConvertible {
     }
 }
 
+//HelpersTests.defaultTestSuite.run()
 MergeTwoSortedLikedListsTest.defaultTestSuite.run()
 
 //: [Next](@next)
