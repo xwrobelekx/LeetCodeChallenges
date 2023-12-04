37a8c1601520288d9d319a206481542ad223bf1f
Kamil Wrobel <wrobelk@live.com>
2023-12-03 19:58:21 -0600

    Unique emails my solution

---
+++ LeetCodeChallenges.playground/Pages/929. Unique Emails - Easy.xcplaygroundpage/Contents.swift
@@ -0,0 +1,58 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 929. Unique emails
+
+// MARK: - My Solution
+func numUniqueEmails(_ emails: [String]) -> Int {
+    var uniqueEmails = Set<String>()
+
+    for email in emails {
+        // string work
+        let splitEmail = email.split(separator: "@")
+        var localName = splitEmail[0]
+        var domainName = splitEmail[1]
+        
+        var plusIndex = localName.firstIndex(of: "+")
+        var localNameWithRemovedPlus = localName.split(separator: "+")[0]
+        
+        localNameWithRemovedPlus = localNameWithRemovedPlus.filter { $0 != "." }
+        
+        let newMail = String(localNameWithRemovedPlus + "@" + domainName)
+        uniqueEmails.insert(newMail)
+    }
+
+    return uniqueEmails.count
+}
+
+
+// MARK: - Other Solutions
+
+
+// MARK: - Tests
+import XCTest
+
+class UniqueEmailTests: XCTestCase {
+    func testRegularEmails() {
+        let emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+        XCTAssertEqual(numUniqueEmails(emails), 3)
+    }
+    
+    func testEmailsWithDots() {
+        let emails = ["test.email+alex@leetcode.com",
+                      "test.e.mail+bob.cathy@leetcode.com",
+                      "testemail+david@lee.tcode.com"]
+        XCTAssertEqual(numUniqueEmails(emails), 2)
+    }
+
+    
+    func testEmails2() {
+        let emails = ["test.email+alex@leetcode.com", "test.email.leet+alex@code.com"]
+        XCTAssertEqual(numUniqueEmails(emails), 2)
+    }
+}
+
+UniqueEmailTests.defaultTestSuite.run()
+
+//: [Next](@next)
