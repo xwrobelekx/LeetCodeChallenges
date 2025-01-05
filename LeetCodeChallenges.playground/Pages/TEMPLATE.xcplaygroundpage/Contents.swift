45a5e29d393bf3653be579ea567e716325038a27
Kamil Wrobel <wrobelk@live.com>
2025-01-05 15:06:39 -0500

    Created prefilled template

--- LeetCodeChallenges.playground/Pages/TEMPLATE.xcplaygroundpage/Contents.swift
+++ LeetCodeChallenges.playground/Pages/TEMPLATE.xcplaygroundpage/Contents.swift
@@ -3,6 +3,25 @@
 import Foundation
 
 
+// Name
+// Link:
 
+// MARK: My Solution
+
+
+// MARK: - Other Solutions
+
+
+// MARK: - Tests
+import XCTest
+
+class ExampleTest: XCTestCase {
+    func testExample() {
+        XCTAssertEqual("", "Hello, World!")
+    }
+}
+
+
+ExampleTest.defaultTestSuite.run()
 
 //: [Next](@next)
